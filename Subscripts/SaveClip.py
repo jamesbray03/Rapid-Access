a804bdaf168740fa5bfae37c02edef689bb1d1cd
James Bray <47334864+James-Bray19@users.noreply.github.com>
2023-08-07 20:52:02 +0100

    Update SaveClip.py
    adjusted for general user

--- Subscripts/SaveClip.py
+++ Subscripts/SaveClip.py
@@ -2,30 +2,23 @@ import os
 from PIL import ImageGrab
 from datetime import datetime
 
-# if 
+# Screenshot folder path
+folder_path = f"C:\Users\{os.getlogin()}\OneDrive\Pictures\Screenshots"
+
+# Saves image in clipboard to specified screenshot folder
 def save_clipboard_image(folder_path):
+
     # Check if the folder exists, create it if necessary
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
 
-    # Get image data from clipboard
+    # Get image from clipboard
     image = ImageGrab.grabclipboard()
-
-    # Check if clipboard contains an image
     if image is None:
-        print("No image found in clipboard.")
         return
 
-    # Generate a unique filename with a timestamp for the image
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    filename = os.path.join(folder_path, f"screenshot_{timestamp}.png")
-
-    # Save the image to the specified folder
-    image.save(filename)
-    print(f"Image saved as: {filename}")
-
-# Specify the folder path where you want to save the image
-folder_path = r"C:\Users\mango\OneDrive\Pictures\Screenshots"
+    # Save the image with a unique file name 
+    image.save(os.path.join(folder_path, f"screenshot_{datetime.now().strftime('%Y%m%d_%H%M%S')}.png"))
 
 # Call the function to save the clipboard image
 save_clipboard_image(folder_path)
